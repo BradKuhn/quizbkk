8e29f463c4e47dd62d78ccc92c2f742ea39aea71
Brad <bradkuhn@live.com>
2015-07-06 22:27:20 +0000

    reinitialized files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Quizbkk::Application.config.secret_key_base = 'fdf7e54e437cc8d53e4b24221159986434f0079336126b394606e9a9813071aea389cb0a412fa19b0cafa5dfac96f95d73a1a470b4a40f28ec28db7e4f5e2f20'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Quizbkk::Application.config.session_store :cookie_store, key: '_quizbkk_session'
